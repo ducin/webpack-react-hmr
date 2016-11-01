1517039bedb896ad70d2a0227a37326f63c9c750
Tomasz Ducin <tomasz.ducin@gmail.com>
2016-11-01 13:33:42 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webpack-react-hmr
+
+Loosely based on [this article](https://robots.thoughtbot.com/setting-up-webpack-for-react-and-hot-module-replacement).
---
+++ app/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {render} from 'react-dom';
+
+import Greeting from "./greeting";
+
+render(
+  <Greeting name="World"/>,
+  document.body
+);
